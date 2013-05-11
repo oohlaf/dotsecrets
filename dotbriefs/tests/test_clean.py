61af21c1081e36aa6f90fc909d719e5ec0d44bd8
Olaf Conradi <olaf@conradi.org>
2013-05-11 23:52:06 +0200

    Add first test case for clean

---
+++ dotbriefs/tests/test_clean.py
@@ -0,0 +1,24 @@
+import unittest
+
+from clean import CleanSecret
+
+
+class TestCleanSecret(unittest.TestCase):
+
+    def setUp(self):
+        self.secrets = []
+        self.secrets.append(CleanSecret('passwd',
+            r'password(\s*)=(\s*)(?#UpToHash)',
+            r'password\1=\2(?#Key)',
+            'Mutt passwords',
+            True))
+
+    def test_state(self):
+        state = self.secrets[0].__getstate__()
+        other_secret = CleanSecret('', '', '')
+        other_secret.__setstate__(state)
+        self.assertEqual(self.secrets[0].__dict__, other_secret.__dict__)
+
+
+if __name__ == '__main__':
+    unittest.main()
