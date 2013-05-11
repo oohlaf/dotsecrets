7c80d47040e4551844c3f28d551e72d63cc64625
Olaf Conradi <olaf@conradi.org>
2013-05-11 22:59:22 +0200

    Add arguments for logging

--- dotbriefs/dotbriefs.py
+++ dotbriefs/dotbriefs.py
@@ -9,13 +9,22 @@ from smudge import smudge
 
 
 logger = logging.getLogger()
+
 logging_configured = False
+
+log_levels = {
+        'debug'    : logging.DEBUG,
+        'info'     : logging.INFO,
+        'warning'  : logging.WARNING,
+        'error'    : logging.ERROR,
+        'critical' : logging.CRITICAL
+        }
 
 
 def configure_logging(args):
     global logging_configured
     if not logging_configured:
-        logger.setLevel(logging.DEBUG)
+        logger.setLevel(log_levels[args.log_level])
         console_log = logging.StreamHandler(stream=sys.stderr)
         logger.addHandler(console_log)
         logging_configured = True
@@ -24,25 +33,29 @@ def configure_logging(args):
 def main():
     parser = argparse.ArgumentParser(
             description='Manage dot files with secrets')
+    parser.add_argument('--log-level', metavar='LEVEL',
+            choices=log_levels.keys(), default='warning',
+            help="set logging to LEVEL, where LEVEL is "
+                 "one of %(choices)s; default is %(default)s")
     subparsers = parser.add_subparsers()
 
     file_parser = argparse.ArgumentParser(add_help=False)
     file_parser.add_argument('--input', metavar='FILE',
             type=argparse.FileType('r'), default=sys.stdin,
-            help="Read input from FILE, default is '-' stdin")
+            help="read input from FILE, default is '-' stdin")
     file_parser.add_argument('--output', metavar='FILE',
             type=argparse.FileType('w'), default=sys.stdout,
-            help="Write output from FILE, default is '-' stdout")
+            help="write output from FILE, default is '-' stdout")
 
     parser_clean = subparsers.add_parser('clean', parents=[file_parser])
     parser_clean.add_argument('--config', metavar='FILE',
-            help='Load config from FILE')
+            help='load config from FILE')
     parser_clean.add_argument('type')
     parser_clean.set_defaults(func=clean)
 
     parser_smudge = subparsers.add_parser('smudge', parents=[file_parser])
     parser_smudge.add_argument('--store', metavar='FILE',
-            help='Load secrets from FILE')
+            help='load secrets from FILE')
     parser_smudge.add_argument('type')
     parser_smudge.set_defaults(func=smudge)
     
