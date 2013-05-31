704aa12b2599c54f3feb5279b106c58b41bb1b92
Olaf Conradi <olaf@conradi.org>
2013-05-31 23:17:10 +0200

    Add utils
    Moved CopyTemplate to utils

---
+++ dotbriefs/tests/test_utils.py
@@ -0,0 +1,16 @@
+import unittest
+
+from dotbriefs.utils import CopyTemplate
+
+
+class TestUtils(unittest.TestCase):
+
+    def test_copytemplate(self):
+        """Test copy template"""
+        t = CopyTemplate()
+        l = "This is a line"
+        return self.assertEqual(l, t.sub(l))
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ dotbriefs/utils.py
@@ -0,0 +1,14 @@
+import os
+import stat
+
+
+class CopyTemplate(object):
+    def sub(self, line):
+        return line
+
+
+def is_only_user_readable(filename):
+    """Return true if and only if filename is readable by user and
+    unreadable by group and others."""
+    mode = stat.S_IMODE(os.stat(filename).st_mode)
+    return mode == 0600
