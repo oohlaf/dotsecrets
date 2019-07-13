453d1750f926beb98edaa08da291453115d54b0a
Olaf Conradi <olaf@conradi.org>
2019-07-13 22:36:40 +0200

    Add new init function to configure git attributes
    Also moves more clean parameters to params.py

--- dotsecrets/params.py
+++ dotsecrets/params.py
@@ -13,3 +13,22 @@ TAG_SECRET_END = '$'
 
 # Tag used in regex substitution for secret keys
 TAG_SECRET_KEY = '(?#Key)'
+
+# Regex shortcuts in dotfilters
+DOTFILTERS_KEYWORD_DICT = {
+    # Match a quoted string
+    '(?#QuotedString)':
+        r'("[^"\\]*(?:\\.[^"\\]*)*"|\'[^\'\\]*(?:\\.[^\'\\]*)*\')',
+    # Match an unquoted single word or a quoted string
+    '(?#QuotedOrSingleWord)':
+        r'("[^"\\]*(?:\\.[^"\\]*)*"|\'[^\'\\]*(?:\\.[^\'\\]*)*\'|\S+)',
+    # Match whitespace up to hash symbol
+    '(?#WSUpToHash)':
+        r'([^\s#]+(?:[ \t\v\f]*[^\s#]+)+)',
+    # Match whitespace up to semicolon
+    '(?#WSUpToSemicolon)':
+        r'([^\s;]+(?:[ \t\v\f]*[^\s;]+)+)',
+}
+
+# Pattern used to detect presence of the dotsecrets filter in git attributes
+GIT_ATTR_DOTSECRETS = r'^[^#].*filter=dotsecrets'
